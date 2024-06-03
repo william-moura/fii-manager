b4f949ba923da8864fa5cd98488d1eedd166ef04
Will_Moura <william@febrafar.com.br>
2024-06-03 15:48:18 -0300

    teste de lib

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,14 @@ use App\Domain\Fii\Controllers\CreateFiiController;
 use App\Domain\Fii\Controllers\FiiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use WilliamMoura\PriceFii\PriceFii;
+use WilliamMoura\PriceFii\Providers\FiisProvider;
 
 Route::get('fii', [FiiController::class, 'index']);
 Route::post('fii', CreateFiiController::class);
+Route::get('test', function () {
+   $fii = new PriceFii(
+    new FiisProvider()
+   );
+   return $fii->getFiiPrice('BCFF11');
+});
