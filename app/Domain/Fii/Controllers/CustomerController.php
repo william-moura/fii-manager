e81fbe545f5abe8ccefb9b8e9b4a77fe5c1747ce
Will_Moura <william@febrafar.com.br>
2024-06-03 12:19:31 -0300

    require lib get fii price

---
+++ app/Domain/Fii/Controllers/CustomerController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domain\Fii\Controllers;
+use App\Http\Controllers\Controller;
+
+class CustomerController extends Controller
+{
+    
+}
---
+++ app/Domain/Fii/Models/Customer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Domain\Fii\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+}
